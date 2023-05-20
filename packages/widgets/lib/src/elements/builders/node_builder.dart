5b6f8407db737c5d19b48134ef2cee73b39b5a01
andreabuttarelli <andreabuttarelli.design@gmail.com>
2023-05-20 12:31:07 +0200

    Update node_builder.dart

--- packages/widgets/lib/src/elements/builders/node_builder.dart
+++ packages/widgets/lib/src/elements/builders/node_builder.dart
@@ -1,3 +1,4 @@
+import 'package:device_frame/device_frame.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:theta_design_system/theta_design_system.dart';
@@ -23,23 +24,67 @@ class NodeBuilder extends StatefulWidget {
 }
 
 class _NodeBuilderState extends State<NodeBuilder> {
-  _handleDecorationChange(CNode? focusNode) => (focusNode?.id == widget.node.id)
-      ? BoxDecoration(
-          border: Border.all(width: 2, color: Palette.blue),
-        )
-      : const BoxDecoration();
+  BoxDecoration _handleDecorationChange(CNode? focusNode) =>
+      (focusNode?.id == widget.node.id)
+          ? BoxDecoration(
+              border: Border.all(width: 2, color: Palette.blue),
+            )
+          : const BoxDecoration();
+
+  EdgeInsets _handleMargins(TreeState state) =>
+      (widget.node.getAttributes[DBKeys.margins] as FMargins? ??
+              const FMargins(
+                  margins: [0, 0, 0, 0],
+                  marginsTablet: [0, 0, 0, 0],
+                  marginsDesktop: [0, 0, 0, 0]))
+          .get(state: state, context: context);
+
+  EdgeInsets _handlePadding(TreeState state) =>
+      (widget.node.getAttributes[DBKeys.padding] as FMargins? ??
+              const FMargins(
+                  margins: [0, 0, 0, 0],
+                  marginsTablet: [0, 0, 0, 0],
+                  marginsDesktop: [0, 0, 0, 0]))
+          .get(state: state, context: context);
+
+  bool _handleVisibility(TreeState state) =>
+      (widget.node.getAttributes[DBKeys.visibility] as bool? ?? true) &&
+      (state.deviceType == DeviceType.desktop
+          ? (widget.node.getAttributes[DBKeys.visibleOnDesktop] as bool? ??
+              true)
+          : state.deviceType == DeviceType.tablet
+              ? (widget.node.getAttributes[DBKeys.visibleOnTablet] as bool? ??
+                  true)
+              : (widget.node.getAttributes[DBKeys.visibleOnMobile] as bool? ??
+                  true));
+
+  double _handleRotation(TreeState state) => double.parse(
+      (widget.node.getAttributes[DBKeys.rotation] as FTextTypeInput?)?.value ??
+          '0');
 
   @override
   Widget build(BuildContext context) {
     final state = context.watch<TreeState>();
     if (state.forPlay) return widget.child;
-    return DecoratedBox(
-      decoration: _handleDecorationChange(state.focusedNode),
-      position: DecorationPosition.foreground,
-      child: GestureDetector(
-        onTap: widget.onTap,
-        onPanStart: (e) => widget.onPanStart(),
-        child: widget.child,
+    return Padding(
+      padding: _handleMargins(state),
+      child: DecoratedBox(
+        decoration: _handleDecorationChange(state.focusedNode),
+        position: DecorationPosition.foreground,
+        child: Transform.rotate(
+          angle: _handleRotation(state),
+          child: Visibility(
+            visible: _handleVisibility(state),
+            child: Padding(
+              padding: _handlePadding(state),
+              child: GestureDetector(
+                onTap: widget.onTap,
+                onPanStart: (e) => widget.onPanStart(),
+                child: widget.child,
+              ),
+            ),
+          ),
+        ),
       ),
     );
   }
