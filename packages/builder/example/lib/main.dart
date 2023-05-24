e2c939181ed6db4aaccbee9bbce9fb1ed1f73df5
Andrea Buttarelli <49411143+andreabuttarelli@users.noreply.github.com>
2023-05-24 16:03:13 +0200

    Update main.dart

--- packages/builder/example/lib/main.dart
+++ packages/builder/example/lib/main.dart
@@ -2,9 +2,13 @@ import 'package:flutter/material.dart';
 import 'package:theta/theta.dart';
 
 Future<void> main() async {
+  /// Initialize Theta instance.
+  /// You can get an anonymous key at https://app.buildwiththeta.com
   await Theta.initialize(
-      anonKey:
-          'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyZWYiOiJmNDBhNWRmNS0wZjBmLTQ5NGItYjU0MC05ZDAwODc0ODQzNTQiLCJhbm9uX2tleSI6ImVmZjI3ZDczLTcwYmUtNDIwOS04MGMyLTI5MDJhZWYzOTM4ZCIsImlhdCI6MTY4NDc2NTU4MywiZXhwIjoxNzE2MzIzMTgzLCJpc3MiOiJodHRwczovL2J1aWxkd2l0aHRoZXRhLmNvbSJ9.P7HDAAlw9Ho-ZaTkYP-yBtIY97BcuuGvnR66bxa30nc');
+    cacheEnabled: false,
+    anonKey: 'anonKey',
+  );
+
   runApp(const MyApp());
 }
 
@@ -18,22 +22,38 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
+    /// ThetaProvider is used to provide the project styles to the widgets.
+    /// It's required to use Theta widgets.
     return ThetaProvider(
       theme: ThemeMode.light,
       child: MaterialApp(
         home: Scaffold(
+          /// UIBox is the main widget.
+          /// It's used to build the UI.
+          /// It requires a page [name].
           body: UIBox(
             'Homepage',
+
+            /// [placeholder] is the widget displayed while the page is loading.
             placeholder: () => const CircularProgressIndicator(),
+
+            /// [errorWidget] is the widget displayed if an error occurs.
             errorWidget: (error) => Text(error),
+
+            /// [workflows] are the workflows that can be triggered by the user, mixing no-code and code.
             workflows: [
               Workflow(
-                'Container l3l2s',
-                Trigger.onHover,
+                'node id',
+                Trigger.onMouseEnter,
                 () => debugPrint('Hovered!'),
               ),
               Workflow(
-                'Text a34b8',
+                'node name',
+                Trigger.onMouseExit,
+                () => debugPrint('Not hovered!'),
+              ),
+              Workflow(
+                'node id',
                 Trigger.onTap,
                 () => debugPrint('Tapped!'),
               ),
