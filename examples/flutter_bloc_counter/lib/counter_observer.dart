0270be98a74e55b54c20c4e8b259ac32852fb96b
Andrea Buttarelli <49411143+andreabuttarelli@users.noreply.github.com>
2023-07-22 20:12:01 +0200

    feat(react to changes and /examples folder) (#200)

---
+++ examples/flutter_bloc_counter/lib/counter_observer.dart
@@ -0,0 +1,17 @@
+import 'package:bloc/bloc.dart';
+
+/// {@template counter_observer}
+/// [BlocObserver] for the counter application which
+/// observes all state changes.
+/// {@endtemplate}
+class CounterObserver extends BlocObserver {
+  /// {@macro counter_observer}
+  const CounterObserver();
+
+  @override
+  void onChange(BlocBase<dynamic> bloc, Change<dynamic> change) {
+    super.onChange(bloc, change);
+    // ignore: avoid_print
+    print('${bloc.runtimeType} $change');
+  }
+}
