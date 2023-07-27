58f844b2cb33d351d5f957a990568ee0847b244f
Sailesh Dahal <43571990+saileshbro@users.noreply.github.com>
2023-07-27 14:01:12 +0545

    feat: add stacked state management example (#204)
    * feat: add stacked state management example
    
    * docs: update readme

---
+++ examples/flutter_stacked_counter/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -0,0 +1,16 @@
+//
+//  Generated file. Do not edit.
+//
+
+import FlutterMacOS
+import Foundation
+
+import device_info_plus
+import package_info_plus
+import path_provider_foundation
+
+func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  DeviceInfoPlusMacosPlugin.register(with: registry.registrar(forPlugin: "DeviceInfoPlusMacosPlugin"))
+  FLTPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FLTPackageInfoPlusPlugin"))
+  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
+}
