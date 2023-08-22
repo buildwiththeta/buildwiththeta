9a2985cc1f7db7695b68c40464de53c538239465
andreabuttarelli <andreabuttarelli.design@gmail.com>
2023-08-22 17:44:18 +0200

    feat

--- embed/macos/Flutter/GeneratedPluginRegistrant.swift
+++ embed/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,6 +5,12 @@
 import FlutterMacOS
 import Foundation
 
+import device_info_plus
+import package_info_plus
+import path_provider_foundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  DeviceInfoPlusMacosPlugin.register(with: registry.registrar(forPlugin: "DeviceInfoPlusMacosPlugin"))
+  FLTPackageInfoPlusPlugin.register(with: registry.registrar(forPlugin: "FLTPackageInfoPlusPlugin"))
+  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
 }
