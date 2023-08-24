1dfff881cac817a2fc20309fb51be14eb27e1a11
andreabuttarelli <andreabuttarelli.design@gmail.com>
2023-08-24 15:57:55 +0200

    Update main.dart

--- embed/lib/main.dart
+++ embed/lib/main.dart
@@ -1,17 +1,85 @@
-import 'package:embed/app.dart' deferred as app;
-import 'package:embed/error_screen.dart' deferred as error_screen;
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:theta/theta.dart';
 
-void main() {
+Future<void> main() async {
   try {
     String name = Uri.base.queryParameters["component_name"]!;
     String token = Uri.base.queryParameters["token"]!;
-    app
-        .loadLibrary()
-        .then((value) => runApp(app.MyApp(name: name, token: token)));
+    runApp(MyApp(name: name, token: token));
   } catch (e) {
-    error_screen
-        .loadLibrary()
-        .then((value) => runApp(error_screen.ErrorScreen()));
+    runApp(const MaterialApp(
+      home: Scaffold(
+          body: Center(
+        child: Text('Something went wrong.'),
+      )),
+    ));
+  }
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({
+    super.key,
+    required this.name,
+    required this.token,
+  });
+
+  final String name;
+  final String token;
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: MyHomePage(name: name, token: token),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.name, required this.token});
+
+  final String name;
+  final String token;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  bool isLoaded = false;
+
+  @override
+  void initState() {
+    super.initState();
+    Theta.initialize(
+        anonKey: widget.token,
+        connectionMode: ConnectionMode.continuous,
+        componentsNames: [widget.name]).then((value) {
+      setState(() {
+        isLoaded = true;
+      });
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.transparent,
+      body: !isLoaded
+          ? const Center(child: CupertinoActivityIndicator())
+          : ThetaProvider(
+              theme: ThemeMode.light,
+              child: UIBox(
+                widget.name,
+                placeholder: const Center(child: CupertinoActivityIndicator()),
+              ),
+            ),
+    );
   }
 }
