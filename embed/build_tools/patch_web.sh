06a11d871a8062063b0dc39ff35a85eb92d3cf29
andreabuttarelli <andreabuttarelli.design@gmail.com>
2023-08-24 18:27:32 +0200

    feat

--- embed/build_tools/patch_web.sh
+++ embed/build_tools/patch_web.sh
@@ -2,5 +2,5 @@
 
 set -e
 
-#patch ./build/web/flutter.js < ./build_tools/web/flutter.js.patch
+patch ./build/web/flutter.js < ./build_tools/web/flutter.js.patch
 #patch ./build/web/flutter_service_worker.js < ./build_tools/web/flutter_service_worker.js.patch
