d4bdb179526eaec4e8f80568140fe19d5e7377d2
andreabuttarelli <andreabuttarelli.design@gmail.com>
2023-08-25 16:22:02 +0200

    feat

--- embed/build_tools/patch_web.sh
+++ embed/build_tools/patch_web.sh
@@ -2,7 +2,8 @@
 
 set -e
 
-patch ./build/web/flutter.js < ./build_tools/web/flutter.js.patch
+#patch ./build/web/flutter.js < ./build_tools/web/flutter.js.patch
+cp -f  ./build_tools/web/flutter.js ./build/web/flutter.js
 cp -f  ./build_tools/web/flutter_service_worker_new.js ./build/web/flutter_service_worker.js
 cp -f  ./build_tools/web/AssetManifest.json ./build/web/assets/AssetManifest.json
 cp -f  ./build_tools/web/FontManifest.json ./build/web/assets/FontManifest.json
