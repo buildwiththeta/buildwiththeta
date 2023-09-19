cdce3e3e0d98c72301b5ee7fdbfbc2dca20cafa3
Mehmet Kasap <72016623+kasapiniz@users.noreply.github.com>
2023-09-19 11:48:27 +0300

    fix(component-treeState) (#272)
    * fix(component-treeState)
    
    * fix()
    
    ---------
    
    Co-authored-by: Andrea Buttarelli <49411143+andreabuttarelli@users.noreply.github.com>

--- packages/widgets/lib/src/elements/widgets/component.dart
+++ packages/widgets/lib/src/elements/widgets/component.dart
@@ -41,8 +41,8 @@ class _OpenWComponentState extends State<OpenWComponent> {
     const NodeRendering nodeRendering = NodeRendering();
     final widget0 = nodeRendering.renderTree(componentChildren);
     final globalState = context.watch<TreeState>();
-
     return ChangeNotifierProvider(
+      key: UniqueKey(),
       create: (_) => globalState.copyWith(
           nodeComponentID: widget.state.node.id,
           fit: widget0.getAttributes[DBKeys.componentFit] == 'absolute'
