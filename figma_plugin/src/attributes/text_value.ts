52b4e15aa15cfbcfb55cc92f96849c642286010a
Mehmet Kasap <mehhmet99@gmail.com>
2023-10-19 19:34:27 +0300

    fix(tex-value)

--- figma_plugin/src/attributes/text_value.ts
+++ figma_plugin/src/attributes/text_value.ts
@@ -1,10 +1,11 @@
 import { DBKeys } from "../key_constants";
+import { FTextTypeInput } from "../features/text_type_input";
 
 export function toTextValue(node: SceneNode) {
   if (node.type === "TEXT") {
     const text = node as TextNode;
     return {
-      [DBKeys.value]: text.characters
+      [DBKeys.value]: new FTextTypeInput({ value: text.characters }).toJson()
     };
   }
 }
